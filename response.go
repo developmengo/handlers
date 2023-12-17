e1e500112d3b748fa4bf753ece908ab5f16fcd13
developmen.go <developmen.go@gmail.com>
2023-12-17 13:29:21 +0700

    update response

--- response.go
+++ response.go
@@ -3,9 +3,10 @@ package handlers
 import "github.com/beego/beego/v2/client/orm"
 
 type GeneralResponse struct {
-	Status  int    `json:"status"`
-	Message string `json:"message"`
-	Count   int64  `json:"count"`
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	Count   int64       `json:"count"`
+	Data    interface{} `json:"data"`
 }
 
 type ArrayResponse struct {
